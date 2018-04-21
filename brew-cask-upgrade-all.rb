72a4338e593fd9bbe4a2467d251b6577826d332a
token <token@positrium.org>
2018-04-21 13:30:45 +0900

    fix count with ignore_items

--- brew-cask-upgrade-all.rb
+++ brew-cask-upgrade-all.rb
@@ -5,7 +5,7 @@
 FORCE_UPDATE = ARGV.include?('-f') || ARGV.include?('--force')
 HELP_USAGE = ARGV.include?('-h') || ARGV.include?('--help')
 DRY_RUN = ARGV.include?('-d') || ARGV.include?('--dry-run')
-COUNT_UPDATES = ARGV.include?('-c') || ARGV.include?('--count')
+UPDATE_COUNT_ONLY = ARGV.include?('-c') || ARGV.include?('--count')
 
 if HELP_USAGE
   puts 'usage:'
@@ -14,103 +14,200 @@ if HELP_USAGE
   puts '  -h , --help : this help message.'
   puts '  -f , --force : force upgrade that newest version is "latest".'
   puts '  -d , --dry-run : dry run but execute brew update, brew cleanup.'
-  puts '  -c , --count : count updatable casks.'
+  puts '  -c , --count : count updatable casks. for bitbar plugin: https://github.com/positrium/bitbar-brew-cask-upgrade-all'
   puts ' '
   exit
 end
 
 # update brew
 
-module BrewUpdatable
-  def message(msg)
-    puts "#{msg}" unless COUNT_UPDATES
-  end
-
+module Updatable
   def update
     raise NotImplementedError
   end
+end
 
+module Cleanable
   def cleanup
     raise NotImplementedError
   end
 end
 
 class BrewUpdate
-  include BrewUpdatable
+  include Updatable
+  include Cleanable
 
   def update
-    message 'brew update:'
-    message `brew update`
+    puts 'brew update:'
+    puts `brew update`
 
-    message 'brew upgrade:'
-    message `brew upgrade`
+    puts 'brew upgrade:'
+    puts `brew upgrade`
   end
 
   def cleanup
-    message 'brew cleanup:'
-    message `brew cleanup`
+    puts 'brew cleanup:'
+    puts `brew cleanup`
   end
 end
 
 class CasksUpdate
-  include BrewUpdatable
+  include Updatable
+  include Cleanable
 
-  @ignore_item
+  @ignore_items
+  @items
   attr_reader :update_count
 
   def initialize
-    @ignore_item = [''] #gpgtools']
+    @ignore_items = [''] #gpgtools']
     @update_count = 0
+    @items = `brew cask list`.split("\n")
   end
 
   def update
-    message 'brew cask upgrade:'
+    puts 'brew cask upgrade:'
+<<<<<<< HEAD
 
-    `brew cask list`.split("\n").each do |item|
+    @items.each do |item|
       # ignore when specified it
-      next if @ignore_item.include?(item)
+      next if @ignore_items.include?(item)
 
-      # get most recently version number
-      current = `ls -t /usr/local/Caskroom/#{item}/`.split("\n").first.strip
-      # get latest version number
-      latest = `brew cask info #{item} | grep #{item} | head -n 1`.split(': ')[1].strip
+      if updatable(item)
+        # output differences
+        differently_info(item)
+        update_item(item)
+      end
+    end
 
-      # output differences
-      message "#{item}"
-      message "latest  : #{latest}"
-      message "current : #{current}"
-      message ' '
+  end
 
-      # verify
-      if latest != current
-        if FORCE_UPDATE && latest == 'latest'
-          message "#{item} updating.."
-          message `brew cask uninstall --force #{item}`
-          message `brew cask install #{item}`
-          message "#{item} updated !!!"
-          message ' '
-        elsif DRY_RUN
-          message "#{item} can update !"
-          message ' '
-        elsif COUNT_UPDATES
-          @update_count = @update_count + 1
-          # do nothing
-        else
-          message "#{item} updating.."
-          message `brew cask uninstall --force #{item}`
-          message `brew cask install #{item}`
-          message "#{item} updated !!!"
-          message ' '
+  def count_up_update_items()
+    # silent, no messages.
+    @items.each do |item|
+      # ignore when specified it
+      next if @ignore_items.include?(item)
+
+      @update_count = @update_count + 1 if updatable(item)
+    end
+
+  end
+
+  def force_update()
+    puts 'brew cask force upgrade:'
+=======
+>>>>>>> c5a80bf... refactored
+
+    @items.each do |item|
+      # ignore when specified it
+      next if @ignore_items.include?(item)
+
+      if updatable(item)
+        # output differences
+        differently_info(item)
+<<<<<<< HEAD
+=======
+        update_item(item)
+      end
+    end
+
+  end
+
+  def count_up_update_items()
+    # silent, no messages.
+    @items.each do |item|
+      @update_count = @update_count + 1 if updatable(item)
+    end
+
+  end
+
+  def force_update()
+    puts 'brew cask force upgrade:'
+
+    @items.each do |item|
+      # ignore when specified it
+      next if @ignore_items.include?(item)
+
+      if updatable(item)
+        # output differences
+        differently_info(item)
+>>>>>>> c5a80bf... refactored
+
+        if latest(item) == 'latest'
+          force_update_item(item)
         end
       end
     end
+
+  end
+
+  def dry_run()
+    puts 'brew cask upgrade dry-run:'
+
+    @items.each do |item|
+      # ignore when specified it
+      next if @ignore_items.include?(item)
+
+      if updatable(item)
+        # output differences
+        differently_info(item)
+
+        dry_run_item(item)
+      end
+    end
+
   end
 
   def cleanup
-    message 'brew cask cleanup'
-    message `brew cask cleanup`
+    puts 'brew cask cleanup'
+    puts `brew cask cleanup`
 
-    message "\nall of done.\n"
+    puts "\nall of done.\n"
+  end
+
+  private
+
+  # output differences
+  def differently_info(item)
+    puts "#{item}"
+    puts "latest  : #{latest(item)}"
+    puts "current : #{current(item)}"
+    puts ' '
+  end
+
+  # get most recently version number
+  def current(item)
+    `ls -t /usr/local/Caskroom/#{item}/`.split("\n").first.strip
+  end
+
+  # get latest version number
+  def latest(item)
+    `brew cask info #{item} | grep #{item} | head -n 1`.split(': ')[1].strip
+  end
+
+  def updatable(item)
+    latest(item) != current(item)
+  end
+
+  def force_update_item(item)
+    puts "#{item} updating.."
+    puts `brew cask uninstall --force #{item}`
+    puts `brew cask install #{item}`
+    puts "#{item} updated !!!"
+    puts ' '
+  end
+
+  def dry_run_item(item)
+    puts "#{item} can update !"
+    puts ' '
+  end
+
+  def update_item(item)
+    puts "#{item} updating.."
+    puts `brew cask uninstall --force #{item}`
+    puts `brew cask install #{item}`
+    puts "#{item} updated !!!"
+    puts ' '
   end
 end
 
@@ -119,9 +216,16 @@ brew.update
 brew.cleanup
 
 casks = CasksUpdate.new
-casks.update
-casks.cleanup
 
-if COUNT_UPDATES
+if FORCE_UPDATE
+  casks.force_update
+  casks.cleanup
+elsif DRY_RUN
+  casks.dry_run
+elsif UPDATE_COUNT_ONLY
+  casks.count_up_update_items
   puts casks.update_count
+else
+  casks.update
+  casks.cleanup
 end
